c6bdcd91df3aa3c031730f80276a35d284c9811c
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-10-01 00:32:41 -0400

    added product model requirements

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,42 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { NUMERIC } = require('sequelize');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      DECIMAL:true
+    },
+   stock: {
+    type:DataTypes.INTEGER,
+    allowNull:false,
+    defaultValue: 10,
+    values: NUMERIC
+   },
+   category_id: {
+    type:DataTypes.INTEGER,
+    references: category.id
+   }
+
+
   },
   {
     sequelize,
