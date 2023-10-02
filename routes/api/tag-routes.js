2c03ade8d90ad60b7b8a601ca8e8753fa5493327
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-10-02 00:31:54 -0400

    added get route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,18 +11,35 @@ router.get('/', async (req, res) => {
       include: [{
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
-        through: 'ProductTag',
+        through: ProductTag
       }]
     });
+
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findOne(req.params.id, {
+      include: [{
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        through: ProductTag
+      }]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
