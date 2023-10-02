a23b0e3e3ff7339d46801348a6ac324d00d9803c
Josephonate <131502920+Josephonate@users.noreply.github.com>
2023-10-02 00:55:43 -0400

    seeded data

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,16 +42,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
-});
+// router.post('/', (req, res) => {
+//   // create a new tag
+// });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+// router.put('/:id', (req, res) => {
+//   // update a tag's name by its `id` value
+// });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+// router.delete('/:id', (req, res) => {
+//   // delete on tag by its `id` value
+// });
 
 module.exports = router;
